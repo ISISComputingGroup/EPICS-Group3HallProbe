0370a44b1d0b7c7f826dccf4aa84c187a4b30f26
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-11-02 15:27:36 +0000

    Ruff/pyright tests

--- system_tests/tests/tests.py
+++ system_tests/tests/tests.py
@@ -1,10 +1,10 @@
+# pyright: reportMissingImports=false
 import unittest
 
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
-
+from utils.testing import get_running_lewis_and_ioc
 
 DEVICE_PREFIX = "G3HALLPR_01"
 
@@ -26,6 +26,7 @@ class Group3HallprobeTests(unittest.TestCase):
     """
     Tests for the _Device_ IOC.
     """
+
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("group3hallprobe", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
