55c3b1d120c65373e0e6136c3c5ed01b7f8e5cce
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-11-02 15:18:16 +0000

    Add initial output from device generator

---
+++ system_tests/tests/tests.py
@@ -0,0 +1,34 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+
+
+DEVICE_PREFIX = "G3HALLPR_01"
+
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("G3HALLPR"),
+        "macros": {},
+        "emulator": "group3hallprobe",
+    },
+]
+
+
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+
+
+class Group3HallprobeTests(unittest.TestCase):
+    """
+    Tests for the _Device_ IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc("group3hallprobe", DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+    def test_that_fails(self):
+        self.fail("You haven't implemented any tests!")
